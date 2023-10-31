9b75cddf55c4b3405a52a28f2ef7e3112203491f
anupama4you <43500022+anupama4you@users.noreply.github.com>
2023-11-01 03:19:20 +1030

    backend index.js updated

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ import Replicate from "replicate";
 
 const configuration = new Configuration({
     organization: "org-b3Y1ILtBwy8Bb77EVwceDrf6",
-    apiKey: "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"
+    apiKey: "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"
 });
 
 const replicate = new Replicate({
@@ -34,77 +34,44 @@ connectDB();
 // Routes
 app.use('/', router);
 
-// app.post("/", async (req, res) => {
-//     try {
-//         const { prompt } = req.body;
-//         const modelId = "gpt-3.5-turbo";
-//         const promptText = `${prompt}\n\nResponse:`;
-
-//         // Restore the previous context
-//         for (const [inputText, responseText] of conversationContext) {
-//             currentMessages.push({ role: "user", content: inputText });
-//             currentMessages.push({ role: "assistant", content: responseText });
-//         }
-
-//         // Stores the new message
-//         currentMessages.push({ role: "user", content: promptText });
-
-//         const result = await openai.createChatCompletion({
-//         model: modelId,
-//         messages: currentMessages,
-//         });
-
-//         const responseText = result.data.choices.shift().message.content;
-//         conversationContext.push([promptText, responseText]);
-//         res.send({ response: responseText });
-
-//     } catch (error) {
-//         console.error("Error in POST request:", error);
-//         res.status(400).send({ error: "Bad request" });
-//     }
-// });
-
 app.post("/", async (req, res) => {
     try {
-      // Assuming the filename is sent in the request body
-      const { image, filename } = req.body;
+        const { prompt } = req.body;
+        const modelId = "gpt-3.5-turbo";
+        const promptText = `${prompt}`;
 
-      var realFile = Buffer.from(image,"base64");
+        // Initialize conversation with a system message to set the context
+        const initialMessage = {
+            role: "system",
+            content: "You are a helpful assistant that specializes in agriculture and leaf disease detection who provides short and concise answers for users' questions. If the user’s question is not directly related to these topics, politely redirect them or suggest they seek information elsewhere.",
+        };
 
-      console.log(realFile)
+        // Restore the previous context
+        const currentMessages = [initialMessage];
+        for (const [inputText, responseText] of conversationContext) {
+            currentMessages.push({ role: "user", content: inputText });
+            currentMessages.push({ role: "assistant", content: responseText });
+        }
 
-      if (!filename) {
-        return res.status(400).json({ error: "Filename is missing from the request body" });
-      }
-  
-      // Read the file from the provided filename
-    //   const data = fs.readFileSync(filename);
-        fs.writeFileSync(filename, realFile);
-  
-      // Replace {API_TOKEN} with your actual API token
-      const API_TOKEN = "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";
-  
-      // Make the POST request to the Hugging Face API
-      const response = await fetch("https://api-inference.huggingface.co/models/yusuf802/Leaf-Disease-Predictor", {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${API_TOKEN}`,
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ data: data.toString() }), // Convert the data to a string
-      });
-  
-      if (response.status !== 200) {
-        return res.status(response.status).json({ error: "Failed to get a valid response from the API" });
-      }
-  
-      const result = await response.json();
-      return res.json(result);
+        // Stores the new message
+        currentMessages.push({ role: "user", content: promptText });
+
+        const result = await openai.createChatCompletion({
+            model: modelId,
+            messages: currentMessages,
+            temperature: 0.2
+        });
+
+        const responseText = result.data.choices[0].message.content;
+        conversationContext.push([promptText, responseText]);
+        res.send({ response: responseText });
+
     } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: "Internal server error" });
+        console.error("Error in POST request:", error);
+        res.status(400).send({ error: "Bad request" });
     }
-  });
+});
+
 
 app.listen(port, () => {
     console.log(`chat-gpt API testing app listening at http://localhost:${port})`)
